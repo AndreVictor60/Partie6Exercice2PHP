349d71fed00abb13ea950adb804941220bbd4b6c
AndreVictor60 <andre.victor60@free.fr>
2017-12-12 21:39:16 +0100

    fini

--- index.php
+++ index.php
@@ -6,11 +6,11 @@
   </head>
   <body>
     <?php
-      if (isset($_GET['prenom']) AND isset($_GET['nom']) AND isset($_GET['age'])) // On a le nom et le prénom
+      if (isset($_GET['prenom']) AND isset($_GET['nom']) AND isset($_GET['age']))
       {
       	echo $_GET['prenom'].' '. $_GET['nom'].' '.$_GET['age'];
       }
-      else // Il manque des paramètres, on avertit le visiteur
+      else 
       {
       	echo 'Il y a une erreur !';
       }
